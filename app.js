32851735dcf77ee96a34698eb8ea13005e97ec0c
Lilian Kasem <liliankasem@gmail.com>
2016-10-15 11:49:49 +0100

    seperating long message into two

--- app.js
+++ app.js
@@ -100,7 +100,8 @@ bot.dialog('/help', session => {
 });
 
 bot.dialog('/about', session => {
-    session.endDialog("The game is about guessing your emotions (don't worry! we do not store the photos). We use the Microsoft Emotions API to do this. If you want to play, let me know and I'll give you the emotion you need to act out, all you'll have to do is take a picture and send it to me. If you act out the correct emotion I will fire a request to the lovely sweet machine at the Microsoft Stand and you will get a sweet!");
+    session.send("The game is about guessing your emotions (don't worry! we do not store the photos). We use the Microsoft Emotions API to do this. If you want to play, let me know and I'll give you the emotion you need to act out, all you'll have to do is take a picture and send it to me.");
+    session.endDialog("If you act out the correct emotion I will fire a request to the lovely sweet machine at the Microsoft Stand and you will get a sweet!");
 });
 
 bot.dialog('/play', [
